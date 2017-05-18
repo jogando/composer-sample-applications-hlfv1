42d2bd87ddf9feb993d9c398f37c2e247c2ea278
julian.ogando@yahoo.com.ar <jogando@xter346.local>
2017-05-18 17:08:38 -0300

    commit

--- packages/systest-acceptance/getting-started.sh
+++ packages/systest-acceptance/getting-started.sh
@@ -31,64 +31,31 @@ npm install
 
 npm test
 
-composer network list -p hlfv1 -n digitalproperty-network --enrollId admin --enrollSecret adminpw
+#composer network list -p hlfv1 -n digitalproperty-network --enrollId admin --enrollSecret adminpw
+composer network list -p hlfv1 -n medrec-network --enrollId admin --enrollSecret adminpw
 
 # Leave Composer-GettingStarted back to tmp
 cd "$TMP"
 
-git clone https://github.com/hyperledger/composer-sample-networks
+#git clone https://github.com/hyperledger/composer-sample-networks
+git clone https://github.com/jogando/composer-sample-networks
 
-cd "$TMP/composer-sample-networks/packages/digitalproperty-network"
+#cd "$TMP/composer-sample-networks/packages/digitalproperty-network"
+cd "$TMP/composer-sample-networks/packages/medrec-network"
 
 npm install
 
-TRANSACTION_FILE=$TMP/composer-sample-networks/packages/digitalproperty-network/lib/DigitalLandTitle.js
 
-rm "$TRANSACTION_FILE"
+#composer archive create --sourceType dir --sourceName . --archiveFile digitalproperty-network.bna
+composer archive create --sourceType dir --sourceName . --archiveFile medrec-network.bna
 
-touch "$TRANSACTION_FILE"
-
-cat <<EOF > "$TRANSACTION_FILE"
-/*
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-'use strict';
-
-/** Process a property that is held for sale
- * @param {net.biz.digitalPropertyNetwork.RegisterPropertyForSale} propertyForSale the property to be sold
- * @transaction
- */
-function onRegisterPropertyForSale(propertyForSale) {
-    console.log('### onRegisterPropertyForSale ' + propertyForSale.toString());
-    propertyForSale.title.forSale = true;
-    propertyForSale.title.information = propertyForSale.title.information + ' Updated at: ' + new Date().toDateString();
-
-    return getAssetRegistry('net.biz.digitalPropertyNetwork.LandTitle').then(function(result) {
-            return result.update(propertyForSale.title);
-        }
-    );
-}
-EOF
-
-composer archive create --sourceType dir --sourceName . --archiveFile digitalproperty-network.bna
-
-composer network update --connectionProfileName hlfv1 --archiveFile digitalproperty-network.bna  --enrollId admin --enrollSecret adminpw
+#composer network update --connectionProfileName hlfv1 --archiveFile digitalproperty-network.bna  --enrollId admin --enrollSecret adminpw
+composer network update --connectionProfileName hlfv1 --archiveFile medrec-network.bna  --enrollId admin --enrollSecret adminpw
 
 npm test
 
 cd "$GETTING_STARTED"
 
-npm run submitTransaction
+#npm run submitTransaction
 
 rm -rf "$TMP"
