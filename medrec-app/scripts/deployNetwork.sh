02eea1ab200b165abd4e879b0328bbeda26fae3c
julian.ogando@yahoo.com.ar <jogando@xter346.local>
2017-05-19 09:25:01 -0300

    commit

--- medrec-app/scripts/deployNetwork.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Exit on first error, print all commands.
-set -ev
-
-composer archive create --sourceName medrec-network --sourceType module --archiveFile medrec-network.bna
-composer network deploy --archiveFile medrec-network.bna  -p medrec -i admin -s adminpw
-composer network list -n medrec-network -p medrec -i admin -s adminpw
